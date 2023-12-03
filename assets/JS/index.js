4b9dae7a8d0a185354439b8c14cbc00864cbe533
martinjagos <91561541+martinjagos@users.noreply.github.com>
2023-12-03 22:17:49 +0100

    Add files via upload

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,4 +3,62 @@ $(()=> {
 	$('.mujcarousel').slick();
 });
 
-
+$(document).on('ready', function() {
+      $(".vertical-center-4").slick({
+        dots: false,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      });
+      $(".vertical-center-3").slick({
+        dots: false,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+      $(".vertical-center-2").slick({
+        dots: false,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+      $(".vertical-center").slick({
+        dots: false,
+        vertical: true,
+        centerMode: true,
+      });
+      $(".vertical").slick({
+        dots: false,
+        vertical: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+      $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+      $(".center").slick({
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      });
+      $(".variable").slick({
+        dots: false,
+        infinite: true,
+        variableWidth: true
+      });
+      
+      $(".lazy").slick({
+        lazyLoad: 'ondemand',
+        infinite: true
+      });
+    });
